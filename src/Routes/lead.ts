abc2f7ebbe796d3ca37bcfc340eff1e50ebbd045
sm2101 <sidarthmittal5@gmail.com>
2021-05-17 22:42:55 +0530

    lead routes

--- src/Routes/lead.ts
+++ src/Routes/lead.ts
@@ -1,12 +1,23 @@
-import { Router } from 'express';
-
+import { Router } from "express";
+// import { plainToClass } from 'class-transformer';
+// import { validateObject } from '../utils/utils';
+// import {Lead, LeadModel} from '../Model/Lead';
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'TODO',
-    });
+router.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "TODO",
+  });
 });
-
+// router.get('/lead', async(req,res) =>{
+//     await LeadModel.find({}).then((data) => {
+//         res.json(data);
+//     }).catch((err) =>{
+//         res.status(500).json(err);
+//     });
+// });
+// router.get('/lead/:id', async(req,res) =>{
+//     await LeadModel.findById({req.params.id})
+// })
 export default router;
