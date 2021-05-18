5736e57c719bca412e927863cedd483a9474d83f
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-18 20:17:20 +0550

    fix(lead.ts): remove redundant endpoint

--- src/Routes/lead.ts
+++ src/Routes/lead.ts
@@ -1,19 +1,12 @@
-import { Router } from 'express';
-import { getAllLeads, getLeadById, createLead } from '../Controller/lead';
+import { Router } from "express";
+import { getAllLeads, getLeadById, createLead } from "../Controller/lead";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'TODO',
-    });
-});
+router.get("/", getAllLeads);
 
-router.get('/', getAllLeads);
+router.get("/:id", getLeadById);
 
-router.get('/:id', getLeadById);
-
-router.post('/', createLead);
+router.post("/", createLead);
 
 export default router;
