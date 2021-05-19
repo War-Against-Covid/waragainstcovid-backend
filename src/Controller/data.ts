538a429cdfc9f901b0670ffaf93facebc500dc2b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-05-19 11:17:15 +0550

    feat(data.ts, data.ts): add endpoint to get all cities grouped by state

--- src/Controller/data.ts
+++ src/Controller/data.ts
@@ -1,24 +1,55 @@
 import { Request, Response } from 'express';
 import CITIES from '../utils/cities.json';
 
+function getStates() {
+    let states = [];
+    // here the states will be repeated, so we cast it into a Set and then back into an
+    // array
+    states = [...new Set(CITIES.map((data) => data.state.trim()))];
+    return states;
+}
+
+function getCities() {
+    let cities = [];
+    cities = CITIES.map((data) => data.city.trim());
+    return cities;
+}
+
+function getCitiesGroupedByState() {
+    const obj: { [key: string]: Array<string> } = {};
+    const states = getStates();
+    states.forEach((state) => {
+        const stateArray: string[] = [];
+        CITIES.forEach((data) => {
+            if (data.state === state) {
+                stateArray.push(data.city);
+            }
+        });
+        obj[state] = stateArray;
+    });
+    return obj;
+}
+
 /* return an array of all indian cities */
 export function getAllCities(_req: Request, res: Response) {
-    let cities = [];
-    cities = CITIES.map((data) => data.city);
     res.json({
-        cities,
+        cities: getCities(),
         status: 'success',
     });
 }
 
 /* get an array of all indian states */
 export function getAllStates(_req: Request, res: Response) {
-    let states = [];
-    // here the states will be repeated, so we cast it into a Set and then back into an
-    // array
-    states = [...new Set(CITIES.map((data) => data.state))];
     res.json({
-        cities: states,
+        cities: getStates(),
+        status: 'success',
+    });
+}
+
+/* get an object of all cities grouped by states */
+export function getAllCitiesGroupedByState(_req: Request, res: Response) {
+    res.json({
+        cities: getCitiesGroupedByState(),
         status: 'success',
     });
 }
--- src/Routes/data.ts
+++ src/Routes/data.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getAllCities, getAllStates } from '../Controller/data';
+import {
+    getAllCities,
+    getAllCitiesGroupedByState,
+    getAllStates,
+} from '../Controller/data';
 
 const router = Router();
 
 router.get('/all-cities', getAllCities);
+router.get('/all-cities-grouped', getAllCitiesGroupedByState);
 router.get('/all-states', getAllStates);
 
 export default router;
