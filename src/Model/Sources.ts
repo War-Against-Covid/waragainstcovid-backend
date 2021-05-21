83349bbcb8123cd7ecea6c643dce26621a154bde
dramikei <raghav2011rv@gmail.com>
2021-05-21 18:54:42 +0530

    Update POST /api/lead

---
+++ src/Model/Sources.ts
@@ -0,0 +1,13 @@
+import { Expose } from 'class-transformer';
+import { getModelForClass, prop } from '@typegoose/typegoose';
+import { IsNotEmpty } from 'class-validator';
+import DocumentCT from './Base';
+
+export class Sources extends DocumentCT {
+    @prop({ required: true, type: String })
+    @Expose()
+    @IsNotEmpty()
+    public sourceName: string;
+}
+
+export const SourcesModel = getModelForClass(Sources);
