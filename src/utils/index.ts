10b0fb99077064199ddfc5770d19c8b8dbd16216
dramikei <raghav2011rv@gmail.com>
2021-05-30 18:58:14 +0530

    validateError fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { ValidationError } from 'admin-bro';
 import { validate, ValidatorOptions } from 'class-validator';
+import { logger } from './logger';
 import RequestError from './RequestError';
 
 export const validateObject = async (object: object, validatorOptions?: ValidatorOptions) => {
@@ -11,13 +12,14 @@ export const validateObject = async (object: object, validatorOptions?: Validato
 
 export const validateAdminBro = async (object: object, validatorOptions?: ValidatorOptions) => {
     const errors = await validate(object, validatorOptions);
-    if (errors) {
+    if (errors.length > 0) {
         const err: {[key: string]: any} = {};
         errors.forEach((error) => {
             err[error.property] = {
                 message: error.toString(),
             };
         });
+        logger.error(err);
         throw new ValidationError(err);
     }
 };
